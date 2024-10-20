de4c0ae0bbec7da56b36acc393c05d3d4ab44aec
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2024-10-20 15:16:10 +0200

    Add another parachain template for uniques

--- templates/parachain_two/pallets/template/src/mock.rs
+++ templates/parachain_two/pallets/template/src/mock.rs
@@ -1,14 +1,6 @@
-use cumulus_primitives_core::ParaId;
-use sp_core::H256;
-
-use frame_support::{derive_impl, weights::constants::RocksDbWeight, parameter_types, traits::Everything};
+use frame_support::{derive_impl, weights::constants::RocksDbWeight};
 use frame_system::{mocking::MockBlock, GenesisConfig};
-use sp_runtime::{traits::ConstU64, traits::BlakeTwo256, BuildStorage, traits::IdentityLookup};
-
-use pallet_balances::AccountData;
-use frame_system as system;
-
-type Block = frame_system::mocking::MockBlock<Test>;
+use sp_runtime::{traits::ConstU64, BuildStorage};
 
 // Configure a mock runtime to test the pallet.
 #[frame_support::runtime]
@@ -30,30 +22,20 @@ mod test_runtime {
 	#[runtime::pallet_index(0)]
 	pub type System = frame_system;
 	#[runtime::pallet_index(1)]
-	pub type XcnftModule = crate;
-
+	pub type TemplateModule = crate;
 }
+
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
-impl system::Config for Test {
+impl frame_system::Config for Test {
 	type Nonce = u64;
 	type Block = MockBlock<Test>;
 	type BlockHashCount = ConstU64<250>;
 	type DbWeight = RocksDbWeight;
-	type AccountData = AccountData<u128>;
-
-}
-pub type XcmRouter = ();
-
-parameter_types! {
-	/// Relay Chain `TransactionByteFee` / 10
-	pub const proposal_time_in_blocks_parameter: u32 = 200000;
 }
 
 impl crate::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
-	type RuntimeCall = RuntimeCall;
-	type XcmSender = XcmRouter;
-	type ProposalTimeInBlocks = proposal_time_in_blocks_parameter;
+	type WeightInfo = ();
 }
 
 // Build genesis storage according to the mock runtime.
